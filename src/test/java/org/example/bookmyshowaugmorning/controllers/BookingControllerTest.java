e44f7a1abb865fe77fa4490cd69eee8dfb90eb43
Saharsh97 <saharsh.singh_1@scaler.com>
2024-11-05 19:13:56 +0530

    test update

--- src/test/java/org/example/bookmyshowaugmorning/controllers/BookingControllerTest.java
+++ src/test/java/org/example/bookmyshowaugmorning/controllers/BookingControllerTest.java
@@ -12,13 +12,9 @@ import org.example.bookmyshowaugmorning.services.BookingService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
